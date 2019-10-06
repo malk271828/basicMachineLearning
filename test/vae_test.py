506dd470b6491fb7c58b66ea9196bca6ff1d7e1c
Masato Tsuchiya <malk271828@gmail.com>
2019-10-06 23:44:51 +0900

    update test

--- test/vae_test.py
+++ test/vae_test.py
@@ -10,10 +10,11 @@ from sklearn.datasets import *
 from sklearn.metrics import classification_report
 from sklearn.preprocessing import MinMaxScaler
 from keras.optimizers import Adam
+from tensorflow.keras.backend import get_value
 
 from vae import *
 
-def test_1d_binary():
+def test_breast_cancer():
     """binary classification test
     Reference:
         https://www.programcreek.com/python/example/104690/sklearn.datasets.load_breast_cancer
@@ -21,21 +22,29 @@ def test_1d_binary():
     X, y = load_breast_cancer(return_X_y=True)
     assert len(X) == len(y)
     input_shape = X[0].shape
+    original_dim = np.prod(X[0].shape)
+    enable_mse = False
     print("\n--------------------------------------")
     print("%d samples" % len(X))
     print("input_shape:", input_shape)
     print("--------------------------------------")
     scaler = MinMaxScaler()
     scaler.fit(X)
+    transformed_X = scaler.transform(X)
     enable_graph = False
 
-    vae, encoder, decoder = build_vae(input_shape, enable_mse=False,
+    vae, encoder, decoder, vae_loss = build_vae(input_shape, enable_mse=enable_mse,
                                         enable_graph=enable_graph)
-    vae.compile(optimizer=Adam(0.0002, 0.5))
-    vae.fit(scaler.transform(X), batch_size=128, epochs=300)
-    reconstruct_X = vae.predict(X)
+    vae.compile(optimizer=Adam(0.0002, 0.5), loss=vae_loss)
+    vae.fit(transformed_X, transformed_X, batch_size=128, epochs=300)
 
-    #print(classification_report(y, predicted_y))
+    # Test
+    reconstruct_X = vae.predict(transformed_X)
+    z_mean, z_log_var, _ = encoder.predict(transformed_X)
+    print(transformed_X)
+    print(reconstruct_X)
+    loss_instance = vae_loss(y_pred=reconstruct_X, y_true=transformed_X)
+    print(get_value(loss_instance))
 
 def test_mnist():
     enable_mse = False
@@ -66,11 +75,13 @@ def test_mnist():
     if enable_graph:
         plot_model(vae, to_file='vae_mlp.png', show_shapes=True)
 
-    # train the autoencoder
+    # train the variational autoencoder
+    # feed the same tensor for X and y:
+    # https://github.com/tensorflow/tensorflow/issues/21894
     vae.fit(x_train, x_train,
             epochs=epochs,
             batch_size=batch_size,
-            validation_data=(x_test, None))
+            validation_data=(x_test, x_test))
     vae.save_weights('vae_mlp_mnist.h5')
 
     plot_results(models,
