d57cce29964b29bd05ac6bb225f4b2e844ae5f45
Masato Tsuchiya <malk271828@gmail.com>
2020-01-18 15:24:07 +0900

    refactoring at fileSelector

--- lombardFileSelector.py
+++ lombardFileSelector.py
@@ -2,13 +2,23 @@ from glob import glob
 
 BASE_DIR = "./"
 
-class lombardFileSelector:
-    def __init__(self, base_dir=BASE_DIR):
+class fileSelector:
+    def __init__(self,
+                 base_dir:str = BASE_DIR) -> list:
         self.base_dir = base_dir
+
+    def getFileList(self):
+        path = self.base_dir
+        return glob(path)
+
+class lombardFileSelector(fileSelector):
+    def __init__(self,
+                 base_dir:str = BASE_DIR):
+        super().__init__(base_dir)
 
     def getFileList(self,
                     domain:str,
-                    verbose:int=0):
+                    verbose:int = 1):
         if domain == "visual":
             domain = "front"
             ext = "mov"
