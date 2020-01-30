bigfix: slice incorrect dimension

gradcam.py
@@ -9,7 +9,7 @@ import cv2
 from keras import backend as K
 from keras.models import load_model, Model
 
-def grad_cam(input_model,
+def gradcam(input_model,
              image: np.array,
              cls: int,
              layer_name: str,
@@ -31,7 +31,7 @@ def grad_cam(input_model,
     https://github.com/eclique/keras-gradcam/blob/master/grad_cam.py
     https://towardsdatascience.com/using-tf-print-in-tensorflow-aa26e1cff11e
     """
-    y_c = input_model.output[0, cls]
+    y_c = input_model.output[:, :, cls]
     conv_output = input_model.get_layer(layer_name).output
     grads = K.gradients(y_c, conv_output)[0]
 
@@ -40,16 +40,8 @@ def grad_cam(input_model,
     gradient_function = K.function([input_model.input], [conv_output, grads])
 
     output, grads_val = gradient_function([image])
-    output, grads_val = output[0, :], grads_val[0, :, :, :]
-
-    weights = np.mean(grads_val, axis=(0, 1))
-    cam = np.dot(output, weights)
-
-    if verbose > 0:
-        print("cls:{0}".format(cls))
-        print("shape of y_c:{0}".format(y_c.get_shape()))
-        print("shape of conv_output:{0}".format(conv_output.get_shape()))
-        tf.print(y_c, [pred_y], output_stream=sys.stderr)
+    weights = np.mean(grads_val[0], axis=(0, 1))
+    cam = np.dot(output[0], weights)
 
     # Process CAM
     cam = cv2.resize(cam, image.shape[1:3], cv2.INTER_LINEAR)
@@ -57,4 +49,12 @@ def grad_cam(input_model,
     cam_max = cam.max() 
     if cam_max != 0: 
         cam = cam / cam_max
+
+    if verbose > 0:
+        print("cls:{0}".format(cls))
+        print("shape of y_c:{0}".format(y_c.get_shape()))
+        print("shape of conv_output:{0}".format(conv_output.get_shape()))
+        print("cam_max:{0}".format(cam_max))
+        tf.print(y_c, [pred_y], output_stream=sys.stderr)
+
     return cam

test/ssd_keras_test.py
@@ -34,8 +34,6 @@ from data_generator.object_detection_2d_photometric_ops import ConvertTo3Channel
 from data_generator.object_detection_2d_geometric_ops import Resize
 from data_generator.object_detection_2d_misc_utils import apply_inverse_transforms
 
-from grad_cam import *
-
 # original header
 from cv_util import *
 from gradcam import *
@@ -181,7 +179,7 @@ def test_inference(kerasSSD, visualization, entry, target_layer_names, target_la
             pass
 
 @pytest.mark.parametrize("entry", [0, 1, 2, 3, 4])
-@pytest.mark.parametrize("target_layer_names", [["conv2_2", "conv3_3", "conv4_2", "conv4_3", "conv5_3", "conv7_2"]])
+@pytest.mark.parametrize("target_layer_names", [["conv2_2", "conv3_3", "conv4_3", "conv5_3", "conv6_2", "conv7_2", "conv8_2", "conv9_2"]])
 @pytest.mark.parametrize("target_layer", [4])
 def test_grad(kerasSSD,
               visualization,
@@ -205,11 +203,11 @@ def test_grad(kerasSSD,
 
     for target, class_name in enumerate(classes):
         pred_y = model.predict(preprocessed_input)
-        gradcam = grad_cam( model, preprocessed_input, target, target_layer_names[target_layer],
+        cam = gradcam( model, preprocessed_input, target, target_layer_names[target_layer],
                             verbose=1,
                             pred_y=pred_y)
 
-        jetcam = visualization.cm(gradcam)
+        jetcam = visualization.cm(cam)
         overlayed_array = (jetcam[:,:,:orig_image.shape[2]]*128+orig_image/2.0).astype(np.uint8)
         overlayed_img = Image.fromarray(overlayed_array)
         overlayed_img.save(visualization.output_dir + "/group{0}_{1}_".format(target, class_name) + target_layer_names[target_layer] + "_gradcam.jpg")