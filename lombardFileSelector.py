7960faf6eaff528dfb9e612dbee0cfc257dd556a
Masato Tsuchiya <malk271828@gmail.com>
2020-04-10 19:01:08 +0900

    update

--- lombardFileSelector.py
+++ lombardFileSelector.py
@@ -1,15 +1,4 @@
-from glob import glob
-
-BASE_DIR = "./"
-
-class fileSelector:
-    def __init__(self,
-                 base_dir:str = BASE_DIR) -> list:
-        self.base_dir = base_dir
-
-    def getFileList(self):
-        path = self.base_dir + "/*"
-        return glob(path)
+import fileSelector
 
 class lombardFileSelector(fileSelector):
     def __init__(self,
