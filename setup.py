8d1fa39e683f4f623dae158100c07b00f80c783d
Masato Tsuchiya <malk271828@gmail.com>
2020-04-10 19:27:33 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mlbase",
+    version="0.1",
+    packages=find_packages(),
+    description=("machine learning"),
+    author="M.T"
+)
